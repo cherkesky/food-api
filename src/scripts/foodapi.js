74acb3bf2f6724f768f9f0b97a59de590257c860
Cherkesky <guy@cherkesky.com>
2019-10-15 15:44:51 -0500

    wrote the basic logic that fetch a json file interate through it and print it to the DOM

--- src/scripts/foodapi.js
+++ src/scripts/foodapi.js
@@ -1,6 +1,33 @@
-console.log("linked");
+console.log("Lets play with food");
 
-fetch("http://localhost:8088/food")
-.then(foods => foods.json())
-.then(parsedFoods =>
-  console.table(parsedFoods));
+const foodContainerSelector = document.querySelector("#food-container")
+
+const url = "http://localhost:8088/food"
+
+const printToDOM = (foodFactory) =>{
+  console.log("printToDOM is being called")
+ foodContainerSelector.innerHTML += foodFactory
+}
+
+const foodFactory = (foods) =>{
+console.log("foodFactory is being called")
+return `<div class="container"> 
+<h3>${foods.name}</h3>
+<h4>${foods.ethnicity}</h4>
+<h4>${foods.category}</h4>`
+}
+
+fetch(url)
+  .then(foods => foods.json())
+  .then(parsedFoods => {
+    console.log("before foreach")
+    parsedFoods.forEach(foods => {
+      printToDOM(foodFactory(foods));
+    });
+  })
+  .then(console.log("done running"))
+
+
+
+
+
